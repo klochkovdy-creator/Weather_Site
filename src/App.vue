ee858d3a36b7f2586be01c8081bc0ea320a29ea5
daniil <klsdsedfwe>
2026-04-24 11:05:38 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const test = () => {
+  alert("I was pressed!") 
+}
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <button @click="test">Кнопка <button>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
